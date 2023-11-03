5a0f7b2dd9ec7770b63ddc5a51b3f2e0b01027d2
Dede Kurniawan <dede.klender@gmail.com>
2023-11-03 20:18:32 +0700

    update library.config.js

--- library.config.js
+++ library.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/components/GitProfile.jsx'),
-      name: 'GitProfile',
+      name: 'CraXID',
       fileName: (format) => `gitprofile.${format}.js`,
     },
     rollupOptions: {
