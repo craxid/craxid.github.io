820ce29474585ae2c6f1883aa79744d83c524388
Dede K. <dede.klender@gmail.com>
2025-10-08 00:38:09 +0700

    update script.js

--- script.js
+++ script.js
@@ -122,9 +122,3 @@ const yearSpan = document.getElementById('currentYear');
             yearSpan.textContent = currentYear;
         }
         
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
-.then(reg => console.log('Service Worker registered:', reg))
-.catch(err => console.error('Service Worker failed:', err));
-});
