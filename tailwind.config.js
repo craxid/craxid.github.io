a613a9a73cb13da2018c7394370432fd78e3b17e
Dede K. <dede.klender@gmail.com>
2025-10-08 01:09:31 +0700

    update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  darkMode: 'class',content: ['./index.html', './script.js'],
+  theme: {
+    extend: {
+      colors: {
+        maroon: {
+          600: '#800000',
+          700: '#660000',
+}
+},
+      animation: {
+        'gradient-x': 'gradient-x 5s ease infinite',
+},
+      keyframes: {
+        'gradient-x': {
+          '0%': { 'background-position': '0% 50%'},
+          '50%': { 'background-position': '100% 50%'},
+          '100%': { 'background-position': '0% 50%'},
+}
+}
+}
+},
+  plugins: [],
+}
