e9d8777f66405c420ea1fb241384c8cf231b4ee8
Admin <dev@crax.my.id>
2026-05-19 21:26:44 +0700

    Upload project lokal ke GitHub@craxid/craxid.github.io

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
+/**const CONFIG = {
   accentColor: "#2d6a4f",
   webName: "CraXID Project",
   repositoryName: "craxid-project",
@@ -17,6 +17,42 @@ const CONFIG = {
 
   githubUsername: "craxid",
   whatsappUrl: "/bot/",
+
+  maxProjects: 3,
+
+  techStacks: [
+    "JavaScript",
+    "GitHub",
+    "CSS",
+    "Bot Development",
+    "Node.js",
+    "React",
+    "HTML",
+  ],
+};
+**/
+
+const CONFIG = {
+  accentColor: "#7C5CFF",
+  webName: "CraXID Project",
+  repositoryName: "craxid-project",
+
+  siteUrl: "https://craxid.github.io/",
+  profileImage: "https://github.com/craxid.png",
+
+  description:
+    "Portofolio modern berisi project, eksperimen, dan karya digital berbasis web, bot, dan teknologi kreatif.",
+
+  seoDescription:
+    "CraXID Project adalah portofolio modern berisi project web, bot, automation, dan teknologi kreatif.",
+
+  keywords:
+    "CraXID, portfolio, developer, bot whatsapp, web developer, github portfolio",
+
+  githubUsername: "craxid",
+
+  whatsappUrl: "https://chat.whatsapp.com/ISI_LINK_GRUP_KAMU",
+  botGroupUrl: "https://chat.whatsapp.com/ISI_LINK_GRUP_BOT_KAMU",
 
   maxProjects: 3,
 
